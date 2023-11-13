dae46f10a84e4b0b1aeb134922e105ad7374ae8c
segerberg <andreassegerberg@gmail.com>
2023-11-13 16:08:24 +0100

    Export report

--- app/routes.py
+++ app/routes.py
@@ -1,14 +1,17 @@
 #!/usr/bin/env python
 import datetime
+import json
+import os
 import uuid
 
-from flask import render_template, session, request, copy_current_request_context, flash, redirect, url_for, jsonify
+from flask import render_template, session, request, send_file, flash, redirect, url_for, jsonify
 from app import app, db
 from app.forms import SurveyForm, RegistrationForm, LoginForm, AddSurveyForm, ActivateSurveyForm
 from app.models import User, Survey, Response
 from flask_login import current_user, login_user, logout_user, login_required
-from app.utils import send_mail_confirmation
+from app.utils import send_mail_confirmation, calc_dim, mean, create_pdf
 from sqlalchemy.exc import IntegrityError
+from werkzeug.utils import secure_filename
 
 
 @app.route('/')
@@ -59,13 +62,14 @@ def surveys():
 @app.route('/surveys/<id>')
 @login_required
 def survey_detail(id):
+    min_responses = int(os.getenv('MIN_RESPONSES',5))
     activate_survey_form = ActivateSurveyForm()
     survey = db.session.query(Survey).filter(Survey.user_id == current_user.id).filter(Survey.id == id)\
         .join(Response, Response.survey_id==id).first()
     if not survey:
         survey = db.session.query(Survey).filter(Survey.user_id == current_user.id).filter(Survey.id == id).first()
 
-    return render_template('survey_detail.html', survey=survey, activate_survey_form=activate_survey_form)
+    return render_template('survey_detail.html', survey=survey, activate_survey_form=activate_survey_form, min_responses=min_responses)
 
 @app.route('/toggle-survey/<id>', methods=['GET','POST'])
 @login_required
@@ -130,21 +134,91 @@ def respond(survey_id):
 def respond_post(survey_id):
     form = SurveyForm()
     # if form.validate_on_submit(): # todo
-    data = Response(survey_id=survey_id, created=datetime.datetime.utcnow(), data=form.data)
-    db.session.add(data)
-    db.session.commit()
-    return redirect(url_for('thankyou'))
-
+    survey = db.session.query(Survey).filter(Survey.id == survey_id).first()
+    if survey:
+        data = Response(survey_id=survey_id, created=datetime.datetime.utcnow(), data=form.data)
+        db.session.add(data)
+        db.session.commit()
+        return redirect(url_for('thankyou'))
+    return "403"  # todo better response
 
 @app.route('/thankyou/', methods=['GET'])
 def thankyou():
     return render_template('thankyou.html')
 
 
-@app.route('/report', methods=['GET'])
-def report():
+
+@app.route('/report/<survey_id>', methods=['GET'])
+def report(survey_id):
+    survey = db.session.query(Survey).filter(Survey.user_id == current_user.id).filter(Survey.id == survey_id)\
+        .join(Response, Response.survey_id==survey_id).first()
+
+    LP_calc_vals = (4.4955,	0.9928)
+    LI_calc_vals = (3.6558,	1.0021)
+    LR_calc_vals = (4.5322,	0.8862)
+    GE_calc_vals = (4.2341,	0.8786)
+    GL_calc_vals = (4.1484,	0.8658)
+    GR_calc_vals = (4.4804,	0.9194)
+    GH_calc_vals = (4.0300,	1.0427)
+
+    LP_lst = []
+    LI_lst = []
+    LR_lst = []
+    GE_lst = []
+    GL_lst = []
+    GR_lst = []
+    GH_lst = []
+
+
+    for resp in survey.responses:
+        LP = [resp.data['q1a'],resp.data['q1b'],resp.data['q1c'],resp.data['q1d']]
+        LP_lst.append(mean(LP))
+
+        LI = [resp.data['q1e'], resp.data['q1f'], resp.data['q1g']]
+        LI_lst.append(mean(LI))
+
+        LR = [resp.data['q1h'], resp.data['q1i'], resp.data['q1j'], resp.data['q1k']]
+        LR_lst.append(mean(LR))
+
+        GE =  [resp.data['q2a'],resp.data['q2b'],resp.data['q2c'],resp.data['q2d']]
+        GE_lst.append(mean(GE))
+
+        GL =  [resp.data['q2e'],resp.data['q2f'],resp.data['q2g'],resp.data['q2h']]
+        GL_lst.append(mean(GL))
+
+        GR =  [resp.data['q2i'],resp.data['q2j'],resp.data['q2k'],resp.data['q2l']]
+        GR_lst.append(mean(GR))
+
+        GH =  [resp.data['q2m'],resp.data['q2n'],resp.data['q2o'],resp.data['q2p']]
+        GH_lst.append(mean(GH))
+
+
+
+    L_Prioritet = calc_dim(mean(LP_lst),LP_calc_vals[0],LP_calc_vals[1])
+    L_Involvering = calc_dim(mean(LI_lst),LI_calc_vals[0],LI_calc_vals[1])
+    L_Rattvisa = calc_dim(mean(LR_lst), LR_calc_vals[0], LR_calc_vals[1])
+
+    G_Engagemang = calc_dim(mean(GE_lst), GE_calc_vals[0], GE_calc_vals[1])
+    G_Larande = calc_dim(mean(GL_lst), GL_calc_vals[0], GL_calc_vals[1])
+    G_Regel = calc_dim(mean(GR_lst), GR_calc_vals[0], GR_calc_vals[1])
+    G_Hotbild = calc_dim(mean(GH_lst), GH_calc_vals[0], GH_calc_vals[1])
+
     data = {
-        "respondents": 23,
-        "group_name":"Förebildsförvaltningen"
+        "respondents": survey.responses.count(),
+        "group_name":survey.name,
+        "L_Prioritet":L_Prioritet,
+        "L_Involvering":L_Involvering,
+        "L_Rattvisa":L_Rattvisa,
+        "G_Engagemang":G_Engagemang,
+        "G_Larande":G_Larande,
+        "G_Regel":G_Regel,
+        "G_Hotbild":G_Hotbild
     }
-    return render_template('report.html', data=data)
+    pdf_bytes = create_pdf(data)
+    safe_filename = secure_filename(f'ISKIP resultat från enkätundersökning_{survey.name}')
+    return send_file(
+        pdf_bytes,
+        mimetype='application/pdf',
+        as_attachment=True,
+        download_name=f'{safe_filename}.pdf'
+    )
--- app/utils.py
+++ app/utils.py
@@ -1,6 +1,12 @@
 from flask_mail import Message, Mail
 from flask import render_template
 import os
+import pdfkit
+from jinja2 import Environment, FileSystemLoader
+import plotly.graph_objects as go
+from plotly.io import to_image
+import base64
+from io import BytesIO
 
 
 
@@ -31,3 +37,56 @@ def calc_dim(val, normmean, stddev):
     """
     result = 30 * ((val - normmean) / stddev) + 100
     return result
+
+
+def create_chart(name,L_Prioritet,L_Involvering,L_Rattvisa,G_Engagemang,G_Larande,G_Regel,G_Hotbild):
+    fig_data = [
+        go.Scatterpolar(
+            fill='none',
+            name=name,
+            r=[L_Prioritet, L_Involvering, L_Rattvisa, G_Engagemang, G_Larande, G_Regel, G_Hotbild],
+            theta=['L_Prioritet', 'L_Involvering', 'L_Rättvisa', 'G_Engagemang', 'G_Lärande', 'G_Regel-legitimitet',
+                   'G_Hotbild'],
+            type='scatterpolar'
+        ),
+        go.Scatterpolar(
+            fill='none',
+            name='Riksurval',
+            r=[100, 100, 100, 100, 100, 100, 100],
+            theta=['L_Prioritet', 'L_Involvering', 'L_Rättvisa', 'G_Engagemang', 'G_Lärande', 'G_Regel-legitimitet',
+                   'G_Hotbild'],
+            type='scatterpolar'
+        )
+    ]
+    # Layout settings
+    fig_layout = go.Layout(
+        showlegend=True,
+        polar=dict(
+            radialaxis=dict(angle=90, visible=True, showline=False, range=[50, 150])
+        )
+    )
+    fig = go.Figure(data=fig_data, layout=fig_layout)
+    width = 600
+    height = 400
+    image_bytes = to_image(fig, format='png',width=width, height=height, scale=2, engine='kaleido')
+    image_base64 = base64.b64encode(image_bytes).decode('utf-8')
+    return image_base64
+
+
+def create_pdf(data):
+    file_loader = FileSystemLoader('./app')
+    env = Environment(loader=file_loader)
+    template = env.get_template('templates/report.html')
+    b64 = create_chart(data['group_name'],data['L_Prioritet'], data['L_Involvering'],data['L_Rattvisa'],data['G_Engagemang'], data['G_Larande'], data['G_Regel'], data['G_Hotbild'])
+
+    output = template.render(data=data, b64=b64)
+    opts = {'javascript-delay': 1000,
+            'no-stop-slow-scripts': None,
+            'debug-javascript': None,
+            "enable-local-file-access": ""}
+    pdf_bytes = pdfkit.from_string(output, False, options=opts)
+
+    return BytesIO(pdf_bytes)
+
+
+
