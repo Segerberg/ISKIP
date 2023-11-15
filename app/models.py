f669713f6471166008f1268943fc5adf3c304fd4
segerberg <andreassegerberg@gmail.com>
2023-11-15 13:57:31 +0100

    use lambda to create uuid pk

--- app/models.py
+++ app/models.py
@@ -48,7 +48,7 @@ class User(UserMixin, db.Model):
 
 
 class Survey(db.Model):
-    id = db.Column(db.String,primary_key=True, default=str(uuid.uuid4()))
+    id = db.Column(db.String,primary_key=True, default=lambda: str(uuid.uuid4()))
     name = db.Column(db.String(128), index=True, unique=True)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
     active = db.Column(db.Boolean)
